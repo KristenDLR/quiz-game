749add998b495e30d882fabc8772e63d36092d56
KristenDLR <delarosa.kristen@gmail.com>
2022-02-12 15:47:05 -0800

    updated highscores page

---
+++ assets/js/highscores.js
@@ -0,0 +1,12 @@
+ // either get scores from localstorage or set to empty array
+function showHighScores(){
+ // either get scores from localstorage or set to empty array
+  var
+}
+   // sort highscores by score property in descending order
+
+   // create li tag for each high score
+   // display on page
+
+
+// run function when page loads
