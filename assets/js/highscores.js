b0831418c3180a0715aa1ab6c31500edfee0baf2
Kristen De La Rosa <kristendelarosa@Kristens-MacBook-Pro.local>
2022-02-20 20:18:54 -0800

    completed stylig and highscore page functionality

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,12 +1,44 @@
  // either get scores from localstorage or set to empty array
-function showHighScores(){
+
+ let stats = document.getElementById("stats");
+ let orderList = document.createElement("ol");
+ 
+ function showHighScores(){
+   
  // either get scores from localstorage or set to empty array
-  var
-}
+  let allscores = JSON.parse(window.localStorage.getItem("allscores"));
+  console.log("All scores: " + allscores[0].score);
+
+       allscores.sort((a, b) => {
+         return b.score -a.score;
+       });
+   hscores= JSON.stringify(allscores);
+   console.log("In Order: " + hscores);
+   console.log("all scores: " + allscores[0].initials)
    // sort highscores by score property in descending order
+       for(i = 0; i < allscores.length; i++){
 
-   // create li tag for each high score
-   // display on page
-
+         if(i==10) {
+            break;
+         }else{
+     
+          let listScores = allscores[i].score;
+            console.log("score: " + listScores);
+   
+           let listNames = allscores[i].initials.toUpperCase();
+             console.log("score: " + listScores);
+   
+          // create li tag for each high score
+         // display on page
+           
+            hsList = document.createElement("li");
+            orderList.appendChild(hsList);
+            hsList.innerHTML = listNames + " : " + listScores;
+            stats.appendChild(orderList);
+        }
+  
+      }
+ }
 
 // run function when page loads
+showHighScores();
